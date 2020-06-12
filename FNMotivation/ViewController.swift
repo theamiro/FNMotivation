09c56c45e4f5ed402c5e988ee46cea0d5afc1813
Michael Amiro <odindo.michael@gmail.com>
2020-06-12 19:23:59 +0300

    Initial Commit

---
+++ FNMotivation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FNMotivation
+//
+//  Created by Michael Amiro on 12/06/2020.
+//  Copyright © 2020 Michael Amiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
